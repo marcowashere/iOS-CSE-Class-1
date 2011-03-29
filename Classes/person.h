500e1137b0a174f5b19e74ab6bcd160c90278a06
Marco <marco@Marcos-MacBook-Air.local>
2011-03-29 15:48:51 +1100

    Adding empty person classes

---
+++ Classes/person.h
@@ -0,0 +1,16 @@
+//
+//  person.h
+//  Class1
+//
+//  Created by Marco on 29/03/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface person : NSObject {
+
+}
+
+@end
---
+++ Classes/person.m
@@ -0,0 +1,14 @@
+//
+//  person.m
+//  Class1
+//
+//  Created by Marco on 29/03/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "person.h"
+
+
+@implementation person
+
+@end
